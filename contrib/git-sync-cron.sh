34af33ed4d3b178116b2fce98194632126f2995f
Ram Balachandran <ramb@spiraldatagroup.com.au>
2023-07-07 21:28:03 +0930

    minor updates to cron script

--- contrib/git-sync-cron.sh
+++ contrib/git-sync-cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run this script in cronjob by editing crontab -e and following line
 # @reboot /bin/bash <path/to/this/script>
-export GIT_SYNC_DIRECTORY="${HOME}/my_codes/github_repos/my_repos/codesScripts"
+export GIT_SYNC_DIRECTORY="<path-to-your-git-repo>"
 export GIT_SYNC_COMMAND="/usr/local/bin/git-sync"
 export GIT_SYNC_INTERVAL=900
 /bin/bash /usr/local/bin/git-sync-on-inotify
